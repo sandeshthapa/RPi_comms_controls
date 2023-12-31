9c7a41fc1b5f906c36d91a0308b052dc71397c62
Sandesh Thapa <thapasandesh1@gmail.com>
2023-12-31 14:35:10 -0500

    Updated uncessary files

--- RPi_Simulink/rpi_basic_example_ert_rtw/codertarget_assembly_flags.mk
+++
@@ -1,5 +0,0 @@
-ASFLAGS_ADDITIONAL = 
-TARGET_LOAD_CMD = 
-TARGET_LOAD_CMD_ARGS = 
-TARGET_PKG_INSTALLDIR = /home/sandesh/Documents/MATLAB/SupportPackages/R2022b/toolbox/realtime/targets/raspi
-STACK_SIZE = 64
